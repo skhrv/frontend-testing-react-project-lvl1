4bbc736c28afd34f66bb34441c57a585d703d474
Ruslan <notebook1v@gmail.com>
2021-06-09 21:47:52 +0300

    fix result object

--- src/index.js
+++ src/index.js
@@ -80,6 +80,7 @@ export default (url, outputPath = process.cwd()) => {
         .then(() => saveFile(pathToHtml, $.html()))
         .then(() => {
           log(`Page was successfully downloaded into '${outputPath}'`);
+          return { filepath: outputPath };
         });
     })).catch((e) => {
       log(e);
